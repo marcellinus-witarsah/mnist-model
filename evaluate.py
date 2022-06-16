e8cfdf4c1e65271be698cc96128f015b9a66aaed
marcellinus-witarsah <aw.marcel@gmail.com>
2022-06-16 21:42:41 +0700

    Create evaluate.py

---
+++ evaluate.py
@@ -0,0 +1,42 @@
+import tensorflow as tf
+import PIL
+import pandas as pd
+import numpy as np
+import tensorflow_datasets as tfds
+import scipy
+import os
+import matplotlib.pyplot as plt
+
+
+def model_diagnostics(history):
+    """
+    Function to show accuracy and loss of training and validation.
+    """
+    # plot accuracy
+    plt.figure(figsize=(10, 10))
+    plt.subplot(211)
+    plt.title('Accuracy')
+    plt.plot(history['sparse_categorical_accuracy'], color='blue', label='training')
+    plt.plot(history['val_sparse_categorical_accuracy'], color='orange', label='validation')
+    plt.legend()
+    plt.xlabel('Epochs')
+    plt.ylabel('Sparse Categorical Accuracy')
+
+    # plot loss
+    plt.subplot(212)
+    plt.title('Binary Cross Entropy Loss')
+    plt.plot(history['loss'], color='blue', label='training')
+    plt.plot(history['val_loss'], color='orange', label='validation')
+    plt.legend()
+    plt.xlabel('Epochs')
+    plt.ylabel('Loss')
+    plt.show()
+
+
+if __name__ == "__main__":
+    history_csv_path = os.path.join(os.getcwd(), 'best_1', 'history.csv')
+    with open(history_csv_path, 'r') as f:
+        history_pd = pd.read_csv(f)
+    print(history_pd)
+
+    model_diagnostics(history_pd)
