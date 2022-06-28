2711b3b22dd0cf18f13104cba459343087b828f6
marcellinus-witarsah <aw.marcel@gmail.com>
2022-06-28 23:29:26 +0700

    Add canvas drawing and clear button feature

--- website/src/components/CanvasDrawing.js
+++ website/src/components/CanvasDrawing.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useRef, useState } from "react";
+import { Button } from "react-bootstrap";
 
 function CanvasDrawing() {
   const canvasRef = useRef(null);
@@ -9,47 +10,65 @@ function CanvasDrawing() {
   useEffect(() => {
     const canvas = canvasRef.current;
     const context = canvas.getContext("2d");
-    canvas.style.width = `${window.innerWidth / 50}vw`;
-    canvas.style.height = `${window.innerWidth / 50}vw`;
-    context.lineCap = "round";
-    context.strokeStyle = "black";
-    context.lineWidth = 5;
+    canvas.width = window.innerWidth / 4;
+    canvas.height = window.innerWidth / 4;
     contextRef.current = context;
   }, []);
 
-  const startDrawing = ({nativeEvent}) => {
+  const startDrawing = (event) => {
     // extract offsetX and offsetY from event
-    console.log("stary drawing")
-    const {offsetX, offsetY} = nativeEvent;
+    console.log(event.clientX, event.clientY);
     contextRef.current.beginPath();
-    contextRef.current.moveTo(offsetX, offsetY);
+    contextRef.current.moveTo(
+      event.clientX - canvasRef.current.offsetLeft,
+      event.clientY - canvasRef.current.offsetTop
+    );
+    contextRef.current.lineCap = "round";
+    contextRef.current.lineJoin = "round";
+    contextRef.current.strokeStyle = "black";
+    contextRef.current.lineWidth = 7;
     setIsDrawing(true);
   };
 
   const stopDrawing = () => {
     contextRef.current.closePath();
     setIsDrawing(false);
-  }
+  };
 
-  const draw = ({nativeEvent}) => {
-    if (!isDrawing){
+  const draw = (event) => {
+    if (!isDrawing) {
       return;
     }
-    const {offsetX, offsetY} = nativeEvent;
-    contextRef.current.lineTo(offsetX, offsetY);
+    contextRef.current.lineTo(
+      event.clientX - canvasRef.current.offsetLeft,
+      event.clientY - canvasRef.current.offsetTop
+    );
     contextRef.current.stroke();
-  }
+  };
+
+  const clearCanvas = () => {
+    contextRef.current.clearRect(
+      0,
+      0,
+      canvasRef.current.width,
+      canvasRef.current.height,
+    );
+  };
 
   return (
     <>
       <canvas
-        className="canvas-container bg-light shadow p-3 mb-5 bg-body rounded"
+        className="canvas-container bg-light shadow mb-5 bg-body rounded"
         onMouseDown={startDrawing}
         onMouseUp={stopDrawing}
-        onMouseLeave={stopDrawing}
         onMouseMove={draw}
         ref={canvasRef}
       ></canvas>
+      <div>
+        <Button variant="warning" onClick={clearCanvas}>
+          Clear
+        </Button>
+      </div>
     </>
   );
 }
--- website/src/components/Result.js
+++ website/src/components/Result.js
@@ -1,11 +1,9 @@
 import React, { Component } from "react";
-import { Button } from "react-bootstrap";
 
 export default class Result extends Component {
   render() {
     return (
       <>
-        <Button variant="warning">Clear</Button>
         <div class="inference-result">Result:</div>
       </>
     );
